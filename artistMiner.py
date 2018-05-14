1339d4d7c70490c115129b9e7a902fbed0e5511d
root <root@Debian9-AdamsSpace.net>
2018-05-14 05:02:25 +0000

    submission

--- artistMiner.py
+++ artistMiner.py
@@ -7,8 +7,22 @@ import smtplib
 from email.parser import Parser
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import time
 
 
+def sendMail(body):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = 'Ding!'
+    msg['From'] = 'adamsocialnetworkproject@gmail.com'
+    msg['To'] = 'amontano495@gmail.com'
+    msg.attach(MIMEText(body,'plain'))
+
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.ehlo()
+    s.starttls()
+    s.login('adamsocialnetworkproject@gmail.com','goRY72QcsDZE')
+    s.sendmail('adamsocialnetworkproject@gmail.com', ['amontano495@gmail.com'], msg.as_string())
+    s.quit()
 
 
 def getAlbums(artistID,spotipyInstance):
@@ -37,7 +51,8 @@ def getTracks(albumID,spotipyInstance):
         tracks.extend(results['items'])
 
     for track in tracks:
-        trackList.append(track)
+        fullTrack = spotipyInstance.track(track['id'])
+        trackList.append(fullTrack)
 
     return trackList
 
@@ -57,19 +72,23 @@ def collabFinder(artistName,spotipyInstance,allRappers):
             artists = list()
             trackPop = track['popularity']
 
-            for i in range(len(track['artists'])):
-                artists.append(track['artists'][i]['name'])
+            if len(track['artists']) < 2:
+                continue
 
-            if artistName in artists or artistName in track['name']:
-                artists[:] = [x for x in artists if x != artistName]
+            else:
+                for i in range(len(track['artists'])):
+                    artists.append(track['artists'][i]['name'])
 
-                for artist in artists:
-                    for rapperName in allRappers:
-                        if artist == rapperName:
+                if artistName in artists or artistName in track['name']:
+                    artists[:] = [x for x in artists if x != artistName]
 
-                            collaborators.append(str(artist.encode('utf-8') + ',' + str(trackPop)))
+                    for artist in artists:
+                            if artist in allRappers:
+                                collaborators.append(
+                                        str(artist.encode('utf-8') + 
+                                            ',' + 
+                                            str(trackPop)))
 
-    collaborators = list(set(collaborators))
     return collaborators
 
 def getPop(artistName,spotipyInstance):
@@ -92,6 +111,8 @@ rapperListFile.close()
 
 
 #print collabFinder('Higher Brothers',sp,allRappers)
+#print getPop('Higher Brothers',sp)
+
 edgeList = list()
 nodeList = list()
 errorList= list()
@@ -100,17 +121,28 @@ nodeFile = open('node_list', 'w')
 collabFile = open('edge_list','w')
 errorFile = open('error_list','w')
 
+counter = 0
 for rapper in allRappers:
+    if counter == 288:
+        sendMail('25 percent finished!')
+    elif counter == 577:
+        sendMail('50 percent finished!')
+    elif counter == 866:
+        sendMail('75 percent finished!')
+
     try:
         collabs = collabFinder(rapper,sp,allRappers)
         for entry in collabs:
             edgeList.append(str(rapper + ',' + entry + '\n'))
 
-        nodeList.append(str(rapper + ',' + getPop(rapper,sp) + str(len(collabs))))
+        rapperPopularity = getPop(rapper,sp)
+        nodeList.append(str(rapper + ',' + str(rapperPopularity) + str(len(collabs)) + '\n'))
 
     except Exception, e:
         errorList.append(str(rapper+'\n'))
         continue
+    counter += 1
+
 
 collabFile.writelines(edgeList)
 collabFile.close()
@@ -121,98 +153,4 @@ errorFile.close()
 nodeFile.writelines(nodeList)
 nodeFile.close()
 
-msg = MIMEMultipart('alternative')
-msg['Subject'] = 'Ding!'
-msg['From'] = 'adamsocialnetworkproject@gmail.com'
-msg['To'] = 'amontano495@gmail.com'
-body = 'The mining has completed!'
-msg.attach(MIMEText(body,'plain'))
-
-s = smtplib.SMTP('smtp.gmail.com',587)
-s.ehlo()
-s.starttls()
-s.sendmail('adamsocialnetworkproject@gmail.com', ['amontano495@gmail.com'], msg.as_string())
-s.quit()
-
-'''
-def collabFinder(artistName):
-    allArtists = list()
-    nonRappers = list()
-    albumList = list()
-    trackList = list()
-    results = sp.search(q=artistName,type='artist')
-
-    for albumType in albumTypeList:
-        results = sp.artist_albums(artistID,album_type=albumType)
-        albums = results['items']
-        while results['next']:
-            results = sp.next(results)
-            albums.extend(results['items'])
-
-        for album in albums:
-            #print 'ALBUM: ',album['name'],'-------------------'
-            trackSearch = sp.album_tracks(album['id'])
-            tracks = trackSearch['items']
-            while trackSearch['next']:
-                trackSearch = sp.next(trackSearch)
-                tracks.extend(trackSearch['items'])
-
-            for track in tracks:
-                if track['name'] not in trackList:
-                    trackList.append(track['name'])
-                    artistSearch = track['artists']
-
-                    artistOnTrack = False
-                    for i in range(len(artistSearch)):
-                        if artistName in artistSearch[i]['name']:
-                            artistOnTrack = True
-                            break
-
-                    if len(artistSearch) > 1 and artistOnTrack:
-                        #print 'song: ',track['name'],'----'
-                        #print 'Artists: '
-                        for i in range(len(artistSearch)):
-                            if artistSearch[i]['name'] not in allArtists and artistSearch[i]['name'] not in nonRappers:
-                                #print artistSearch[i]['name']
-                                artist = sp.artist(artistSearch[i]['id'])
-                                #print artist['genres']
-                                    #print artist['name']
-                                #if (any(genre in artist['genres'] for genre in acceptedGenres) or artist['name'] in allRappers) and artistName not in artist['name']:
-                                if (any(genre in artist['genres'] for genre in acceptedGenres)) and artistName not in artist['name']:
-                                    allArtists.append(artistSearch[i]['name'])
-                                else:
-                                    #print artist['name'],' aint a rapper'
-                                    #print artist['genres']
-                                    nonRappers.append(artist['name'])
-
-
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-
-    allArtists = list(set(allArtists))
-    collabFileName = str(''.join(c for c in artistName if c in valid_chars).replace(' ','') + '_collabFile')
-    collabFile = open(collabFileName,'wr')
-
-    for i in range(len(allArtists)):
-        allArtists[i] = artistName + ',' + allArtists[i].encode('utf-8')+'\n'
-
-
-    collabFile.writelines(allArtists)
-    collabFile.close()
-
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = artistName + "'s " +'collaborators'
-    msg['From'] = 'adamsocialnetworkproject@gmail.com'
-    msg['To'] = 'amontano495@gmail.com'
-    body = '\n'.join(allArtists)
-    msg.attach(MIMEText(body,'plain'))
-
-    s = smtplib.SMTP('smtp.gmail.com',587)
-    s.ehlo()
-    s.starttls()
-    s.login('adamsocialnetworkproject@gmail.com','goRY72QcsDZE')
-    s.sendmail('adamsocialnetworkproject@gmail.com', ['amontano495@gmail.com'], msg.as_string())
-    s.quit()
-
-'''
-
-
+sendMail('DING we are done!')
